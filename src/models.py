4acb6e83b64224084e329f7861b2c81a7ec68fd6
jarodc33 <jarodc333@gmail.com>
2020-05-13 18:29:10 -0700

    new attempt

--- src/models.py
+++ src/models.py
@@ -13,6 +13,7 @@ from sklearn.metrics import f1_score
 from keras.models import Sequential
 from keras.layers import Dense, Conv1D, MaxPooling1D, Dropout, LSTM
 from keras.regularizers import l1, l2, l1_l2
+from keras.models import load_model
 
 from src.sliding_window import *
 
@@ -34,6 +35,29 @@ class BasicNN():
         self.model = self._get_model()
 
     
+
+
+
+    def fit(self, X, y, cycles = 5, epochs = 3):
+        sliding_window_X = make_sliding_window_series(X, self.window_size)
+        for _ in range(cycles):
+            print('Cycle ' + str(_ + 1) + '/' + str(cycles))
+            time.sleep(5)
+            X_train, X_test, y_train, y_test = train_test_split(sliding_window_X, y, test_size = 0.35)
+            self.model.fit(X_train, y_train, batch_size = 100, epochs = epochs, validation_data = (X_test, y_test))
+        return self
+
+    
+    def predict(self, X):
+        y_predictions = self.model.predict_classes(make_sliding_window_series(X, self.window_size))
+        return y_predictions
+    
+    
+    def load_model(self, path):
+        self.model = load_model(path)
+        return self
+
+    
     def _get_model(self):
         model = Sequential()
         model.add(Dense(22, input_dim = self.window_size, activation = 'relu'))
@@ -43,49 +67,56 @@ class BasicNN():
         model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         return model
 
-
-    def fit(self, X, y, cycles = 5, epochs = 3):
-        sliding_window_X = make_sliding_window_series(X, self.window_size)
-        for _ in range(cycles):
-            print('Cycle ' + str(_ + 1) + '/' + str(cycles))
-            X_train, X_test, y_train, y_test = train_test_split(sliding_window_X, y, test_size = 0.35)
-            self.model.fit(X_train, y_train, batch_size = 100, epochs = epochs, validation_data = (X_test, y_test))
-        return self
-
-    
-    def predict(self, X):
-        y_predictions = self.model.predict_classes(make_sliding_window_series(X, self.window_size))
-        return y_predictions
-
         
 
 
 class RNN():
-    def __init__(self, window_size):
+    def __init__(self, window_size, features = 1):
         self.window_size = window_size
+        self.features = features
         self.model = self._get_model()
 
     
-    def _get_model(self):
-        return model
-
 
     def fit(self, X, y, cycles = 5, epochs = 3):
-        sliding_window_X = make_sliding_window_series(X, self.window_size)
+        reshaped_X = self._reshape_data(X)
         for _ in range(cycles):
             print('Cycle ' + str(_ + 1) + '/' + str(cycles))
-            X_train, X_test, y_train, y_test = train_test_split(sliding_window_X, y, test_size = 0.35)
-            self.model.fit(X_train, y_train, batch_size = 100, epochs = epochs, validation_data = (X_test, y_test))
+            X_train, X_test, y_train, y_test = train_test_split(reshaped_X, y, test_size = 0.35)
+            self.model.fit(X_train,
+                           y_train,
+                           batch_size = 100,
+                           epochs = epochs,
+                           validation_data = (X_test, y_test)
+                          )
+            time.sleep(120)
         return self
 
     
     def predict(self, X):
-        y_predictions = self.model.predict_classes(make_sliding_window_series(X, self.window_size))
+        y_predictions = self.model.predict_classes(self._reshape_data(X))
         return y_predictions
-        
+    
+    
+    def load_model(self, path):
+        self.model = load_model(path)
+        return self
 
+    
+    def _get_model(self):
+        model = Sequential()
+        model.add(LSTM(self.window_size * 2, input_shape  = (self.window_size, self.features)))
+        model.add(Dense(40, activation = 'relu'))
+        model.add(Dense(11, activation = 'softmax'))
+        model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
+    
+    def _reshape_data(self, X):
+        sliding_window = make_sliding_window(X, self.window_size)
+        return sliding_window.reshape((sliding_window.shape[0], self.window_size, self.features))
+    
+    
 
-        
         
         
         
